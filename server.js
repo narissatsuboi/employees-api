2f7e840807da15fc437f0cb60648c45b40e8df69
Narissa Tsuboi <narissatsuboi@gmail.com>
2023-05-30 15:55:45 -0700

    test route for ec2

--- server.js
+++ server.js
@@ -1,28 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const app = express();
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const app = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(cookieParser());
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
-const startServer = (port=5000) => {
-    mongoose.connection.once('open', () => {
-    //   logger.info(`Connected to MongoDB on default conn`);
-      app.listen(port, () => logger.info(`Server is running on port: ${port}`));
-    //   mongoose.connection.on('disconnect', (err) => logger.error(err));
-    });
-  };
+app.use('/employees', require('./routes/employeeRoutes'))
 
-  const run = async (port=5000) => {
-    try {
-      startServer(port);
-    } catch (err) {
-    //   logger.error('could not run server');
-    //   mongoose.connection.close();
-    }
-  };
-  
-  run();
+const startServer = (port = 5000) => {
+  app.listen(port, () => console.log(`Server is running on port: ${port}`))
+}
+
+const run = async port => {
+  try {
+    startServer(port)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+run(5000)
